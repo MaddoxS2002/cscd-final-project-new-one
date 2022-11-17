11deaac85d6751f128869d4bffa0f215fb933acc
damian <dechevarria@ewu.edu>
2022-11-17 09:36:21 -0800

    Stubbed out SystemManager class

--- src/SystemManager.java
+++ src/SystemManager.java
@@ -1,2 +1,30 @@
 public class SystemManager {
+
+    public Airport createAirport(final String n){
+        return null;
+    }
+
+    public Airline createAirline(final String n){
+
+        return null;
+    }
+
+    public Flight createFlight(final String aname,final String orig,final String dest,final int year,final int month,final int day,final String id){
+
+        return null;
+    }
+
+    public FlightSection createSection(final String air,final String flID,final int rows,final int cols,final SeatClass s){
+        return null;
+    }
+    public void findAvailableFlights(final String orig,final String dest){
+
+    }
+    public void bookSeat(final String air,final String fl,final SeatClass s,final int row,final char col){
+
+    }
+    public void displaySystemDetails(){
+
+    }
+
 }
