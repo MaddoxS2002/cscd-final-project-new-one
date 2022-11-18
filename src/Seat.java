d50cdad4dfb5cd9016e005ee0c2c0d609d6e90d1
Maddox Schoeffler <mschoeffler@ewu.edu>
2022-11-18 09:52:25 -0800

    commit

--- src/Seat.java
+++ src/Seat.java
@@ -1,2 +1,16 @@
 public class Seat {
-}
+
+    private int rowNum;
+    private char col;
+    private boolean isBooked = false;
+
+    public Seat(final int rowNum, final char col){
+        this.rowNum = rowNum;
+        this.col = col;
+        this.isBooked = true;
+    }
+
+    
+
+
+}//end of class
--- src/SeatClass.java
+++ src/SeatClass.java
@@ -1,13 +1,15 @@
 public enum SeatClass {
 
-    economy(1,"A"), business(1, "A"), first(1, "A");
+    economy(1,'A'), business(1, 'A'), first(1, 'A');
 
-    private String col;
+    private char col;
     private int row;
+    private Seat s;
 
-    SeatClass(int row, String col) {
-        this.col = col;
-        this.row = row;
+    SeatClass(int row, char col) {
+        s = new Seat(row,col);
+        /*this.col = col;
+        this.row = row;*/
     }
 
     public int getRow(){
